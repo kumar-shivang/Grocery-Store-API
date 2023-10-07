0d01ec2724eddd518bd1ad2b7bfe06e8b2780dcd
Shivang Kumar <shivangkumar1857@gmail.com>
2023-10-07 16:18:13 +0530

    config module

---
+++ config/__init__.py
@@ -0,0 +1,19 @@
+import os
+from datetime import timedelta
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Config(object):
+    # database config
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(parent_dir, 'database.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # jwt config
+    JWT_SECRET_KEY = 'super-secret'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+    JWT_TOKEN_LOCATION = ['cookies', 'headers']
+    JWT_COOKIE_SECURE = False
+    JWT_COOKIE_CSRF_PROTECT = True
