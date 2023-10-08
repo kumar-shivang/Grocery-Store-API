f22dac1790becf373c0dda6ee86e8aca052e79f1
Shivang Kumar <shivangkumar1857@gmail.com>
2023-10-08 15:15:49 +0530

    added category request route

--- api/managerAPI.py
+++ api/managerAPI.py
@@ -1,7 +1,7 @@
 from error_log import logger
 from database import db
-from database.schema import ProductSchema, UserSchema
-from database.models import Product, Role
+from database.schema import ProductSchema, UserSchema, CategoryRequestSchema
+from database.models import Product, Role, User
 from flask import jsonify, request, make_response, Blueprint
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
@@ -40,6 +40,25 @@ def get_products():
                                  200)
         else:
             return make_response(jsonify({'message': 'No products found'}), 404)
+    except Exception as e:
+        logger.error(e)
+        return make_response(jsonify({'message': str(e)}), 400)
+
+
+@manager_blueprint.route('/request_category', methods=['POST'])
+@jwt_required()
+def request_category():
+    category_request_schema = CategoryRequestSchema(many=False)
+    try:
+        current_user = User.query.get(get_jwt_identity())
+        if current_user.role.role_name == 'manager':
+            body = request.get_json()
+            category_request = category_request_schema.load(body)
+            db.session.add(category_request)
+            db.session.commit()
+            return make_response(jsonify({'message': 'Category requested successfully, wait for approval'}), 200)
+        else:
+            return make_response(jsonify({'message': 'Only managers can request new categories'}), 403)
     except Exception as e:
         logger.error(e)
         return make_response(jsonify({'message': str(e)}), 400)
