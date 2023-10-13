710cae9159165da560503b412cf10407a9dcb64a
Shivang Kumar <shivangkumar1857@gmail.com>
2023-10-13 18:06:13 +0530

    Refactor pyhtml import in templates and update email content
    The pyhtml library is now imported under the alias 'h', and is used throughout mail/templates.py. In addition, email content in manager and category request functions has been updated to be clearer and more specific. This change improves readability of the code and message clarity for users.

--- mail/templates.py
+++ mail/templates.py
@@ -1,95 +1,98 @@
-from pyhtml import *
-
+import pyhtml as h
 
 def manager_created(username:str):
-    msg = html(
-        head(
-            h1('Manager Created')
+    msg = h.html(
+        h.head(
+            h.h1('Manager Request Created')
         ),
-        body(
-            h1('Manager Request Created'),
-            p('Your request has been created successfully, wait for approval'),
-            p('Your username is: ', username),
-            p('Thank you for using grocery store.')
+        h.body(
+            h.h1('Manager Request Created'),
+            h.p('Your manager request has been created successfully'),
+            h.p('Your username is: ', username),
+            h.p('Please wait for approval'),
+            h.p('Thank you for using grocery store.')
         ),
     )
     return msg.render()
 
 
 def manager_approved(user_id:int, username:str):
-    msg = html(
-        head(
-            h1('Manager Approved')
+    msg = h.html(
+        h.head(
+            h.h1('Manager Approved')
         ),
-        body(
-            h1('Manager Request Approved'),
-            p('Your request has been approved successfully'),
-            p('Your username is: ', username),
-            p('Your user id is: ', user_id),
-            p('You can now login to the system'),
-            p('Thank you for using grocery store.')
+        h.body(
+            h.h1('Manager Request Approved'),
+            h.p('Your manager request has been approved successfully'),
+            h.p('Your username is: ', username),
+            h.p('Your user id is: ', user_id),
+            h.p('You can now add products to the store'),
+            h.p('Thank you for using grocery store.')
         ),
     )
     return msg.render()
 
 
 def manager_rejected():
-    msg = html(
-        head(
-            h1('Manager Rejected')
+    msg = h.html(
+        h.head(
+            h.h1('Manager Rejected')
         ),
-        body(
-            h1('Manager Request Rejected'),
-            p('Your request to register as a store manager has been rejected'),
-            p('Thank you for using grocery store.')
+        h.body(
+            h.h1('Manager Request Rejected'),
+            h.p('Your manager request has been rejected'),
+            h.p('Thank you for using grocery store.')
         ),
     )
+
     return msg.render()
 
 
 def category_created(category_name:str):
-    msg = html(
-        head(
-            h1('Category Request Created')
+    msg = h.html(
+        h.head(
+            h.h1('Category Request Created')
         ),
-        body(
-            h1('Category Created'),
-            p('Your category request has been created successfully'),
-            p('Your category name is: ', category_name),
-            p('Please wait for approval'),
-            p('Thank you for using grocery store.')
+        h.body(
+            h.h1('Category Request Created'),
+            h.p('Your category request has been created successfully'),
+            h.p('Your category name is: ', category_name),
+            h.p('Please wait for approval'),
+            h.p('Thank you for using grocery store.')
         ),
     )
+
     return msg.render()
 
 
 def category_approved(category_name:str):
-    msg = html(
-        head(
-            h1('Category Approved')
+    msg = h.html(
+        h.head(
+            h.h1('Category Approved')
         ),
-        body(
-            h1('Category Request Approved'),
-            p('Your category request has been approved successfully'),
-            p('Your category name is: ', category_name),
-            p('You can now add products to this category'),
-            p('Thank you for using grocery store.')
+        h.body(
+            h.h1('Category Request Approved'),
+            h.p('Your category request has been approved successfully'),
+            h.p('Your category name is: ', category_name),
+            h.p('You can now add products to the store'),
+            h.p('Thank you for using grocery store.')
         ),
     )
     return msg.render()
 
 
 def category_rejected():
-    msg = html(
-        head(
-            h1('Category Rejected')
+    msg = h.html(
+        h.head(
+            h.h1('Category Rejected')
         ),
-        body(
-            h1('Category Request Rejected'),
-            p('Your category request has been rejected'),
-            p('Thank you for using grocery store.')
+        h.body(
+            h.h1('Category Request Rejected'),
+            h.p('Your category request has been rejected'),
+            h.p('Thank you for using grocery store.')
         ),
     )
+
     return msg.render()
 
 
