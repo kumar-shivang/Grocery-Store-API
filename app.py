c7315db83f8283171ecac38fc384b824333f61b5
kumar-shivang <shivangkumar1857@gmail.com>
2023-11-06 15:01:41 +0530

    Enable CORS for all routes in Flask API
    Added support for Cross-Origin Resource Sharing (CORS) to all routes in Flask API. It was done by importing CORS, cross_origin from flask_cors and manually adding it to all view functions due to the issue of CORS() not working. This allows requests from all origins, increasing usability for frontend applications hosted on different domains.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from api import init_api
 from database import init_database
 from config import Config
 from mail import init_mail
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 app.config.from_object(Config)
 init_api(app)
 init_database(app)
@@ -12,16 +14,24 @@ init_mail(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return jsonify({'message': 'Hello World!'})
 
 
-@app.route('/routes')
+@app.route('/routes', methods=['GET'])
 def routes():
+    # This is a helper function to get all the routes in the app
     json = {}
     for rule in app.url_map.iter_rules():
         json[rule.endpoint] = rule.rule
     return jsonify(json)
+
+
+# CORS() is not working so I have to add it manually
+for endpoint, view_function in app.view_functions.items():
+    app.view_functions[endpoint] = cross_origin()(view_function)
+else:
+    print('added cors')
 
 
 if __name__ == '__main__':
