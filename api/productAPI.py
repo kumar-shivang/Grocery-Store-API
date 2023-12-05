690bcc910d5ebded59793eaa4aebe10574c43383
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-05 16:50:06 +0530

    Update product fetching function in productAPI.py
    This commit alters the get_products() function to only fetch products which are currently in stock. The update includes changing the query to filter out products without current stock and therefore, improves efficiency by preventing needless calls for out-of-stock items.

--- api/productAPI.py
+++ api/productAPI.py
@@ -13,7 +13,7 @@ from .userAPI import user_blueprint
 def get_products():
     product_schema = ProductSchema(many=True)
     try:
-        products = Product.query.all()
+        products = Product.query.filter(Product.current_stock > 0).all()
         if products:
             return make_response(jsonify({'message': 'Products fetched successfully',
                                           'products': product_schema.dump(products, many=True)}),
