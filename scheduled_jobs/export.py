413085dae64fbb02b392dba93d542bf5a33b0b73
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-12 18:47:04 +0530

    Refactor export_product_as_csv function
    Changed the task decorator in `export_product_as_csv` function from specific 'send_monthly_report' task to a generic celery task. Additionally, the file path was updated for better access, and the 'products_sold' copying was simplified. Lastly, a print statement was added to confirm the successful export of a product.

--- scheduled_jobs/export.py
+++ scheduled_jobs/export.py
@@ -2,12 +2,13 @@ from database.models import Product, Order, Product
 from . import celery
 
 
-@celery.task(name='send_monthly_report')
+@celery.task
 def export_product_as_csv(product_id):
     product = Product.query.filter_by(id=product_id).first()
     orders = Order.query.filter_by(product_id=product_id).all()
     products_sold = len(orders)
-    with open('/static/products/{}.csv'.format(product.id), 'w') as f:
+    with open('static/products/{}.csv'.format(product.id), 'w') as f:
         f.write('Product ID, Product Name, Product Rate, Product Unit, Current Stock, Sold Quantity\n')
-        f.write('{},{},{},{},{},{}\n'.format(product.id, product.name, product.rate, product.unit, product.current_stock, products_sold + " {}s".format(product.unit) if product.unit != "kg" else products_sold + " {}s".format(product.unit)))
+        f.write('{},{},{},{},{},{}\n'.format(product.id, product.name, product.rate, product.unit, product.current_stock,  products_sold))
+    print('exported product {}'.format(product_id))
     return True
