f1ce06811697f6724f1a2b2ff1cd88db66f50aa2
kumar-shivang <shivangkumar1857@gmail.com>
2023-12-14 13:56:46 +0530

    Update SQLALCHEMY_DATABASE_URI path in config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 from datetime import timedelta
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 class Config(object):
     # database config
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(parent_dir, 'database.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'database.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # jwt config
     JWT_SECRET_KEY = 'super-secret'
